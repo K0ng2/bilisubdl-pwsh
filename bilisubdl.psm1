a554ae5eb322c6e8c3c9a17f6b1726a272e34576
K0ng2 <kongpop556677@gmail.com>
2022-07-16 12:35:23 +0700

    fix subtitle url

--- bilisubdl.psm1
+++ bilisubdl.psm1
@@ -1,6 +1,6 @@
 $API_URL = "https://api.bilibili.tv/intl/gateway/web/v2"
-$info_ep = $API_URL + "/ogv/play/{0}?season_id={1}&platform=web"
-$API_episode = $API_URL + "/subtitle?&episode_id="
+$INFO_EP_URL = $API_URL + "/ogv/play/{0}?season_id={1}"
+$EP_URL = $API_URL + "/subtitle?s_locale&episode_id="
 function Get-Bilibili {
   param(
     [string[]]$id,
@@ -10,38 +10,46 @@ function Get-Bilibili {
   )
 
   foreach ($s in $id) {
-    $info = Invoke-RestMethod -Method Get -Uri ($info_ep -f 'season_info', $s)
-    $epList = Invoke-RestMethod -Method Get -Uri ($info_ep -f 'episodes', $s)
+    $info = Invoke-RestMethod -Method Get -Uri ($INFO_EP_URL -f 'season_info', $s)
     $genres = @()
-    foreach ($genre in $info.data.season.styles) {$genres+=$genre.title}
+    foreach ($genre in $info.data.season.styles) { $genres += $genre.title }
     [PSCustomObject]@{
-      Title = $info.data.season.title
-      Genres = $genres -join ', '
+      Title        = $info.data.season.title
+      Genres       = $genres -join ', '
       TotalEpisode = $info.data.season.total_episodes_text
     } | Format-List
+    $epList = Invoke-RestMethod -Method Get -Uri ($INFO_EP_URL -f 'episodes', $s)
     $title = New-CleanText $info.data.season.title
     $null = New-Item -Name $title -ItemType "directory" -Force
     foreach ($section in $epList.data.sections) {
       Write-Host $ep.ep_list_title
       foreach ($ep in $section.episodes) {
         $ep_name = New-CleanText $ep.title_display
-        $filename = $title + '\' + ("{0}.{1}.srt" -f $ep_name, $lang)
+        $filename = Join-Path $title ("{0}.{1}.srt" -f $ep_name, $lang)
         if ((Test-Path $filename) -and !($overwrite)) {
           Write-Warning "$filename : Already exists"
           continue
         }
-        $episode_url = "{0}{1}" -f $API_episode, $ep.episode_id
-        $episode = Invoke-RestMethod -Method Get -Uri $episode_url
-        foreach ($j in $episode.data.subtitles) {
-          if ($lang -eq $j.lang_key) {
-            $sub = Invoke-RestMethod -Method Get -Uri $j.url
-            New-SRT $sub.body | Out-File $filename
-            Write-Host "Writing subtitle: $filename"
-          }
-        }
+        $rawSub = Get-BilibiliEpisode $ep.episode_id $lang
+        New-SRT $rawSub.body | Out-File $filename
+        Write-Host "Writing subtitle: $filename"
       }
     }
   }
+}
+
+function Get-BilibiliEpisode {
+  param(
+    [string]$id,
+    [string]$lang
+  )
+  $episode = Invoke-RestMethod -Method Get -Uri "$EP_URL$id"
+  foreach ($j in $episode.data.subtitles) {
+    if ($lang -eq $j.lang_key) {
+      return Invoke-RestMethod -Method Get -Uri $j.url
+    }
+  }
+  return $null
 }
 
 function New-SRT {
@@ -51,14 +59,14 @@ function New-SRT {
 
   $sub = ''
   for ($i = 0; $i -lt ($body).Length; $i++) {
-    if (($i -ne 0) -or $i -eq ($body).Length) {
+    if (($i -ne 0) -or ($i -eq ($body).Length)) {
       $sub += "`n`n"
     }
     $content = $body[$i].content
-    if ($body[$i].location -eq 2) {
+    $location = $body[$i].location
+    if ($location -eq 2) {
       $line = $content
     } else {
-      $location = $body[$i].location
       $line = "{{\an$location}}$content"
     }
     $timeFrom = "{0:hh\:mm\:ss\,fff}" -f ([timespan]::fromseconds($body[$i].from))
@@ -73,7 +81,7 @@ function New-CleanText {
     [string]$t
   )
   $t = $t -replace '[*|:<>?/\|"]', '_' `
-  -replace "`n", ' '
+    -replace "`n", ' '
   return $t.TrimEnd('.')
 }
 
